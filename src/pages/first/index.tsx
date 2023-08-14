5bd513d420efb9d5725c9df19680100c1c8b8fa6
youjin-10 <105618795+youjin-10@users.noreply.github.com>
2023-08-14 10:07:31 +0900

    initial commit

---
+++ src/pages/first/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import type { InferGetServerSidePropsType, GetServerSideProps } from "next";
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const res = await fetch("https://api.github.com/repos/vercel/next.js");
+  const repo = await res.json();
+  return { props: { repo } };
+};
+
+const Index = ({ repo }) => {
+  return (
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between p-24 $`}
+    >
+      <section>this is index (/first)</section>
+      <section>{repo.stargazers_count}</section>
+    </main>
+  );
+};
+
+export default Index;
---
+++ src/pages/second/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import type { InferGetServerSidePropsType, GetServerSideProps } from "next";
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  // const res = await fetch("https://api.github.com/repos/vercel/next.js")
+  const res = await fetch(
+    "https://api.github.com/repos/vercel/next.js/branches"
+  );
+
+  const branches = await res.json();
+  return { props: { branches } };
+};
+
+const Index = ({ branches }) => {
+  return (
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between p-24 $`}
+    >
+      <section>this is index (/second)</section>
+      <section>{branches[0].name}</section>
+    </main>
+  );
+};
+
+export default Index;
